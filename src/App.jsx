97c607cd88a3b48bbe1111b4899a2cf8d16ed937
devscriptian <NnaaiiDev@gmail.com>
2025-07-13 17:19:13 +0800

    Major Update | project section Halfway

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
+import { useEffect } from "react"
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger"
+
+
 import Header  from "./components/Header"
 import HeroSection from "./components/HeroSection"
 import CustomCursor from "./components/CustomCursor"
 import AboutSection from "./components/AboutSection"
+import ProjectsSection from "./components/ProjectsSection"
 
 export default function App() {
+
+  useEffect(() =>{
+    // Register ScrollTrigger plugin
+    gsap.registerPlugin(ScrollTrigger)
+
+    //Refresh ScrollTrigger when the page is fully loaded 
+    ScrollTrigger.refresh()
+
+    // Clean up ScrollTrigger on component unmount
+    return () => {
+      ScrollTrigger.getAll().forEach((trigger) => trigger.kill ())
+    }
+  }, [])
   return (
     <>
     <Header/>
     <HeroSection/>
     <CustomCursor/>
     <AboutSection/>
+    <ProjectsSection/>
     
     </>
   )
