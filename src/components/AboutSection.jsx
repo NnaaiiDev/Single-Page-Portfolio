b11f969cdf564c8e62e573cf764ad4b41afd8aa6
devscriptian <NnaaiiDev@gmail.com>
2025-07-13 09:11:36 +0800

    changed location

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,18 @@
+
+
+const AboutSection = () => {
+  return (
+    <section className="h-screen relative overflow-auto bg-gradient-to-b from-black to-[#9a74cf50]">
+
+        <div>
+            <h1 className="text-4xl md:text-6xl font-bold sm:mb-16 text-center text-white">
+                About Me
+            </h1>
+        </div>
+
+
+    </section>
+  )
+}
+
+export default AboutSection
